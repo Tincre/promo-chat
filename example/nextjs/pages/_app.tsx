967076f7193f563a25fc78e368117993a9a5fccf
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2023-10-20 15:56:24 -0500

    :recycle: :fire: remove unused wrapper context components and imports

--- example/nextjs/pages/_app.tsx
+++ example/nextjs/pages/_app.tsx
@@ -1,15 +1,8 @@
 import type { AppProps } from 'next/app';
-//import '../../../dist/promo-button.esm.css'; // @tincre/promo-button/bundle.css
-import { TourProvider } from '@reactour/tour';
-import { promoDashboardTourSteps } from '@tincre/promo-dashboard';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <TourProvider steps={promoDashboardTourSteps}>
-      <Component {...pageProps} />
-    </TourProvider>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
