74deb391b2035300105c26ff424cb657ef53a815
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2023-10-30 13:22:51 -0600

    :recycle: :wrench: add in google recaptcha usage #1

--- example/nextjs/pages/_app.tsx
+++ example/nextjs/pages/_app.tsx
@@ -1,8 +1,21 @@
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
-
+import { ReCaptchaProvider } from 'next-recaptcha-v3';
+import dynamic from 'next/dynamic';
+const DynamicReCaptchaProvider = dynamic(() =>
+  import('next-recaptcha-v3').then((module) => module.ReCaptchaProvider)
+);
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    /* @ts-ignore */
+    <DynamicReCaptchaProvider
+      reCaptchaKey="6LdjR8ooAAAAADJdgx5yFve6-45QgTcMyB_51y8V"
+      useEnterprise
+    >
+      {/* @ts-ignore */}
+      <Component {...pageProps} />
+    </DynamicReCaptchaProvider>
+  );
 }
 
 export default MyApp;
