df7bf9dbad76151f497a448ca7515cb42f55ea82
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2023-10-30 12:15:11 -0600

    :recycle: customize css id for error message styling customization

--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -53,7 +53,7 @@ export function Messages({ latestMessages, responseError }: MessagesProps) {
         id={`promo-chat-error-message`}
       >
         <p
-          id="promo-chat-user-message-display"
+          id="promo-chat-error-message-display"
           className="inline-block rounded-lg bg-blue-700 px-4 py-2 text-white"
         >
           {`⚠️ ${responseError} ⚠️`}
