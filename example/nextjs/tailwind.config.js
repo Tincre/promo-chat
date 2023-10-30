c20340f4e784d616603378bb29518baf9de89f91
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2023-10-30 13:20:49 -0600

    :wrench: add animate-wave custom class

--- example/nextjs/tailwind.config.js
+++ example/nextjs/tailwind.config.js
@@ -10,7 +10,23 @@ module.exports = {
     '../../dist/*.{js,ts,jsx,tsx}', // remove for deployed apps
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        wave: {
+          '0%': { transform: 'rotate(0.0deg)' },
+          '15%': { transform: 'rotate(14.0deg)' },
+          '30%': { transform: 'rotate(-8.0deg)' },
+          '40%': { transform: 'rotate(14.0deg)' },
+          '50%': { transform: 'rotate(-4.0deg)' },
+          '60%': { transform: 'rotate(10.0deg)' },
+          '70%': { transform: 'rotate(0.0deg)' },
+          '100%': { transform: 'rotate(0.0deg)' },
+        },
+      },
+      animation: {
+        wave: 'wave 3.0s infinite',
+      },
+    },
   },
   plugins: [],
 };
