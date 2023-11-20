2e412dcd4bc911f816181531a39889c9a2295a6a
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2023-11-20 14:46:45 -0500

    :sparkles: add initial beta pill component

---
+++ src/components/BetaPill.tsx
@@ -0,0 +1,11 @@
+export function BetaPill({}) {
+  return (
+    <span
+      id="promo-chat-header-beta-label"
+      className="inline-flex items-center justify-start rounded-full bg-gray-100 px-2 py-1 text-xs italic text-gray-600 ml-2 mt-2"
+      title="We're trying to make this chat perfect for you so please let us know if you have a suggestion!"
+    >
+      Beta
+    </span>
+  );
+}
