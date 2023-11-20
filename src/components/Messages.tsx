82fe2a7059a75e7440381510afa12f9c6db62c4e
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2023-11-20 17:15:35 -0500

    :sparkles: add select-all to chat dialog messages

--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -29,7 +29,7 @@ export function Messages({ latestMessages, responseError }: MessagesProps) {
           >
             <p
               id="promo-chat-user-message-display"
-              className="inline-block rounded-lg bg-blue-700 px-4 py-2 text-white"
+              className="inline-block rounded-lg bg-blue-700 px-4 py-2 text-white select-all"
             >
               <Markdown remarkPlugins={[remarkGfm]}>{message}</Markdown>
             </p>
@@ -42,7 +42,7 @@ export function Messages({ latestMessages, responseError }: MessagesProps) {
           >
             <p
               id="promo-chat-assistant-message-display"
-              className="inline-block rounded-lg bg-gray-200 px-4 py-2 text-gray-700"
+              className="inline-block rounded-lg bg-gray-200 px-4 py-2 text-gray-700 select-all"
             >
               <Markdown remarkPlugins={[remarkGfm]}>{message}</Markdown>
             </p>
@@ -58,7 +58,7 @@ export function Messages({ latestMessages, responseError }: MessagesProps) {
           >
             <p
               id="promo-chat-error-message-display"
-              className="inline-block rounded-lg bg-blue-700 px-4 py-2 text-white"
+              className="inline-block rounded-lg bg-blue-700 px-4 py-2 text-white select-all"
             >
               <Markdown remarkPlugins={[remarkGfm]}>
                 {`⚠️ ${responseError} ⚠️`}
