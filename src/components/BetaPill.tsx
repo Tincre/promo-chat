ef86ae77dfc0b7e9a7252db44f0e7f5cdb50483c
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2023-11-20 14:57:16 -0500

    :recycle: refactor to support supportEmail input prop

--- src/components/BetaPill.tsx
+++ src/components/BetaPill.tsx
@@ -1,4 +1,8 @@
-export function BetaPill({}) {
+type BetaPillProps = {
+  supportEmail?: string;
+};
+
+function InternalBetaPill() {
   return (
     <span
       id="promo-chat-header-beta-label"
@@ -7,5 +11,18 @@ export function BetaPill({}) {
     >
       Beta
     </span>
+  );
+}
+export function BetaPill({ supportEmail = undefined }: BetaPillProps) {
+  const mailtoEmail =
+    typeof supportEmail !== 'undefined'
+      ? `mailto:${supportEmail}?subject=Chat%20feedback&body=Hi!%20I%20have%20feedback%20regarding%20my%20recent%20chat.%0D%0A%0D%0A%3CTell%20us%20in%20detail%20what%20happened%20and%20what%20you'd%20like%20to%20see%20differently%3E`
+      : undefined;
+  return typeof mailtoEmail === 'undefined' ? (
+    <InternalBetaPill />
+  ) : (
+    <a href={mailtoEmail} target="_blank" rel="noopener noreferrer">
+      <InternalBetaPill />
+    </a>
   );
 }
