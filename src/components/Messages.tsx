354039bfad6a0af2dbe0d1b72c8863aa47d89ff6
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2023-11-21 21:18:30 -0500

    :recycle: update to fix markdown-to-jsx lib

--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -4,8 +4,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
-import Markdown from 'react-markdown';
-import remarkGfm from 'remark-gfm';
+import Markdown from 'markdown-to-jsx';
 
 export type MessagesProps = {
   latestMessages: MessageType[];
@@ -31,7 +30,7 @@ export function Messages({ latestMessages, responseError }: MessagesProps) {
               id="promo-chat-user-message-display"
               className="inline-block rounded-lg bg-blue-700 px-4 py-2 text-white select-all"
             >
-              <Markdown remarkPlugins={[remarkGfm]}>{message}</Markdown>
+              <Markdown>{message}</Markdown>
             </p>
           </div>
         ) : (
@@ -44,7 +43,7 @@ export function Messages({ latestMessages, responseError }: MessagesProps) {
               id="promo-chat-assistant-message-display"
               className="inline-block rounded-lg bg-gray-200 px-4 py-2 text-gray-700 select-all"
             >
-              <Markdown remarkPlugins={[remarkGfm]}>{message}</Markdown>
+              <Markdown>{message}</Markdown>
             </p>
           </div>
         );
@@ -60,9 +59,7 @@ export function Messages({ latestMessages, responseError }: MessagesProps) {
               id="promo-chat-error-message-display"
               className="inline-block rounded-lg bg-blue-700 px-4 py-2 text-white select-all"
             >
-              <Markdown remarkPlugins={[remarkGfm]}>
-                {`⚠️ ${responseError} ⚠️`}
-              </Markdown>
+              <Markdown>{`⚠️ ${responseError} ⚠️`}</Markdown>
             </p>
           </div>
         ) : null}
