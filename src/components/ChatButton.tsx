41ca9eddbd7c60d50f8313382abe2dd7b6f91712
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2023-11-25 09:36:06 -0500

    :sparkles: add ChatButton (before clicked) component

---
+++ src/components/ChatButton.tsx
@@ -0,0 +1,38 @@
+import React, {
+  Dispatch,
+  SetStateAction,
+  MutableRefObject,
+  KeyboardEvent,
+  ReactNode,
+} from 'react';
+
+export type ChatButtonProps = {
+  agentName: string;
+  setIsPromoChatButtonClicked: Dispatch<SetStateAction<boolean>>;
+  userInputRef: MutableRefObject<HTMLInputElement | null>;
+  handleChatButtonEnter: (event: KeyboardEvent<HTMLButtonElement>) => void;
+  children: ReactNode;
+};
+export function ChatButton({
+  agentName,
+  setIsPromoChatButtonClicked,
+  userInputRef,
+  handleChatButtonEnter,
+  children,
+}: ChatButtonProps) {
+  return (
+    <button
+      id="promo-chat-button-not-clicked"
+      tabIndex={1}
+      className="fixed bottom-10 right-10 flex h-16 w-16 animate-wave items-center justify-center rounded-full bg-blue-900 text-2xl text-slate-50 shadow-lg transition duration-300 ease-in-out hover:scale-105 hover:bg-blue-800 hover:shadow-xl dark:bg-blue-100 dark:text-blue-900 hover:dark:bg-blue-200 z-[90]"
+      title={`Chat with ${agentName}`}
+      onClick={() => {
+        setIsPromoChatButtonClicked(true);
+        userInputRef.current?.focus();
+      }}
+      onKeyDown={handleChatButtonEnter}
+    >
+      {children}
+    </button>
+  );
+}
