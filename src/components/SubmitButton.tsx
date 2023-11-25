224854cbbb7007c6931d225d512f769e532987c3
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2023-11-25 11:30:16 -0500

    :sparkles: add component for submit button

---
+++ src/components/SubmitButton.tsx
@@ -0,0 +1,31 @@
+import React, {
+  MouseEvent,
+  KeyboardEvent,
+  MutableRefObject,
+  ReactNode,
+} from 'react';
+export type SubmitButtonProps = {
+  handleChatSubmit: (
+    event: MouseEvent<HTMLButtonElement> | KeyboardEvent<HTMLInputElement>
+  ) => void;
+  submitButtonRef: MutableRefObject<HTMLButtonElement | null>;
+  children?: ReactNode;
+};
+export function SubmitButton({
+  handleChatSubmit,
+  submitButtonRef,
+  children,
+}: SubmitButtonProps) {
+  return (
+    <button
+      tabIndex={3}
+      type="button"
+      ref={submitButtonRef}
+      onClick={handleChatSubmit}
+      className="mt-3 w-full items-center justify-center rounded-md bg-blue-700 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600 sm:ml-3 sm:mt-0 sm:w-auto sm:flex-row"
+      id="promo-chat-input-submit-button"
+    >
+      {children || 'Submit'}
+    </button>
+  );
+}
