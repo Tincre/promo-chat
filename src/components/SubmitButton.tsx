40a1cf9852fff3f93c8080d51c0ac7ff4c30a6a7
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2023-12-07 22:17:16 -0600

    :fire: remove flex-row directive

--- src/components/SubmitButton.tsx
+++ src/components/SubmitButton.tsx
@@ -5,12 +5,7 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-import React, {
-  MouseEvent,
-  KeyboardEvent,
-  MutableRefObject,
-  ReactNode,
-} from 'react';
+import { MouseEvent, KeyboardEvent, MutableRefObject, ReactNode } from 'react';
 export type SubmitButtonProps = {
   handleChatSubmit: (
     event: MouseEvent<HTMLButtonElement> | KeyboardEvent<HTMLInputElement>
@@ -29,7 +24,7 @@ export function SubmitButton({
       type="button"
       ref={submitButtonRef}
       onClick={handleChatSubmit}
-      className="mt-3 w-full items-center justify-center rounded-md bg-blue-700 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600 sm:ml-3 sm:mt-0 sm:w-auto sm:flex-row"
+      className="mt-3 w-full items-center justify-center rounded-md bg-blue-700 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600 sm:ml-3 sm:mt-0 sm:w-auto sm:inline-block"
       id="promo-chat-input-submit-button"
     >
       {children || 'Submit'}
